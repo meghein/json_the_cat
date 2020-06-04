bd0f0ed721e1253b5c3c28d94ce6291225dc07e8
Meghan Hein <meghan.hein@gmail.com>
2020-06-04 19:19:28 +0000

    refactored with callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,31 @@
 const request = require('request');
 
-const name = `${process.argv[2]}`;
+// const breedName = `${process.argv[2]}`;
 
-request('https://api.thecatapi.com/v1/breeds/', (err, _response, body) => {
-  if (err) console.log('error');
-  const data = JSON.parse(body);
-  const catNames = [];
-  for (const cat in data) {
-    if (data[cat].name === name) console.log(data[cat].description);
-    catNames.push(data[cat].name);
-  }
-  if (!catNames.includes(name)) console.log('Breed not found in database');
-});
+
+const fetchBreedDescription = function(breedName, breedDescription) {
+  
+  request('https://api.thecatapi.com/v1/breeds/', (err, _response, body) => {
+    const data = JSON.parse(body);
+    const catNames = [];
+    if (err) {
+      breedDescription(err, null);
+    }
+
+    for (const cat in data) {
+      if (data[cat].name === breedName) {
+        breedDescription(null, data[cat].description);
+        catNames.push(data[cat].name);
+      }
+    }
+    if (!catNames.includes(breedName)) {
+      breedDescription('Breed not found in database', null);
+    }
+  });
+  
+};
+
+
+module.exports = fetchBreedDescription;
 
 
